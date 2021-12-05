ab930d9662d49c9870745833e7696833866e508f
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-05 13:49:49 -0500

    Fixed mint bug and added burnable functionalities

--- contracts/GunnerERC20.sol
+++ contracts/GunnerERC20.sol
@@ -2,17 +2,26 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol"
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract GunnerERC20 is ERC20Capped{
+contract GunnerERC20 is ERC20Burnable, ERC20Capped {
     uint256 constant supply = 100 * 1e6 * 1e18;
+
     constructor(
         address _operations,
         address _emissions,
         address _gameRewards
-    ) ERC20("Gunner","BRO") ERC20Capped(supply){
-        _mint(_operations, (supply * 20 ) / 100) // 20%
-        _mint(_emissions, (supply * 20 ) / 100) // 20%
-        _mint(_gameRewards, (supply * 20 ) / 100) // 60%
+    ) ERC20("Gunner", "BRO") ERC20Capped(supply) {
+        ERC20._mint(_operations, (supply * 20) / 100); // 20%
+        ERC20._mint(_emissions, (supply * 20) / 100); // 20%
+        ERC20._mint(_gameRewards, (supply * 20) / 100); // 60%
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Capped)
+    {
+        super._mint(to, amount);
     }
 }
