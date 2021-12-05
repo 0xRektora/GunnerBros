9acf19d41d101e20f2cb03c5e903d7beedb89167
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-05 13:48:43 -0500

    Updated solc version

--- hardhat.export.ts
+++ hardhat.export.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
     solidity: {
-        version: "0.6.12",
+        version: "0.8.0",
         settings: {
             optimizer: {
                 enabled: true,
