a13db637b1334872b1c6e7bdb1dc69e764d30e6e
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-07 00:48:35 -0500

    Removed constructor logic to separate mint function

--- contracts/GunnerERC20.sol
+++ contracts/GunnerERC20.sol
@@ -8,13 +8,17 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 contract GunnerERC20 is ERC20Burnable, ERC20Capped {
     uint256 constant supply = 100 * 1e6 * 1e18;
 
-    constructor(
+    constructor() ERC20("Gunner", "BRO") ERC20Capped(supply) {}
+
+    function mint(
         address _operations,
         address _emissions,
+        address _airdrop,
         address _gameRewards
-    ) ERC20("Gunner", "BRO") ERC20Capped(supply) {
+    ) public {
         ERC20._mint(_operations, (supply * 20) / 100); // 20%
-        ERC20._mint(_emissions, (supply * 20) / 100); // 20%
+        ERC20._mint(_emissions, (supply * 10) / 100); // 10%
+        ERC20._mint(_airdrop, (supply * 10) / 100); // 10%
         ERC20._mint(_gameRewards, (supply * 20) / 100); // 60%
     }
 
