f4811e26854380c7df42741c13bd604611d36799
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-11 18:20:07 -0500

    Fixed small issues

--- contracts/GunnerERC20.sol
+++ contracts/GunnerERC20.sol
@@ -2,30 +2,28 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract GunnerERC20 is ERC20Burnable, ERC20Capped {
+contract GunnerERC20 is ERC20Burnable, ERC20Capped, Ownable {
     uint256 constant supply = 100 * 1e6 * 1e18;
 
-    constructor() ERC20("Gunner", "BRO") ERC20Capped(supply) {}
+    constructor() ERC20('Gunner', 'BRO') ERC20Capped(supply) {}
 
     function mint(
         address _operations,
         address _emissions,
         address _airdrop,
         address _gameRewards
-    ) public {
-        ERC20._mint(_operations, (supply * 20) / 100); // 20%
-        ERC20._mint(_emissions, (supply * 10) / 100); // 10%
-        ERC20._mint(_airdrop, (supply * 10) / 100); // 10%
-        ERC20._mint(_gameRewards, (supply * 20) / 100); // 60%
+    ) public onlyOwner {
+        _mint(_operations, (supply * 20) / 100); // 20%
+        _mint(_emissions, (supply * 10) / 100); // 10%
+        _mint(_airdrop, (supply * 10) / 100); // 10%
+        _mint(_gameRewards, (supply * 60) / 100); // 60%
     }
 
-    function _mint(address to, uint256 amount)
-        internal
-        override(ERC20, ERC20Capped)
-    {
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Capped) {
         super._mint(to, amount);
     }
 }
