25c474ef3de2f91fe9da111c1c7bdf0ca71ed857
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-11 18:19:34 -0500

    Finished GunnerERC20 tests

--- test/GunnerBrosERC20.test.ts
+++ test/GunnerBrosERC20.test.ts
@@ -1,32 +1,70 @@
-import { ethers } from 'hardhat'
-import { expect } from 'chai'
-import { GunnerERC20 } from '../typechain'
-import { Wallet } from '@ethersproject/wallet'
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { GunnerERC20 } from '../typechain';
+import { Wallet } from '@ethersproject/wallet';
+
+const getAddresses = async () => {
+  const [operationsWallet, gunnerTokenDistributor, gunnerTokenAirdrop, gunnerEmission] = [...Array(4).keys()].map((e) =>
+    ethers.Wallet.createRandom(),
+  );
+  const contract = await (await ethers.getContractFactory('GunnerERC20')).deploy();
+
+  return {
+    deployer: (await ethers.getSigners())[0],
+    contract,
+    operationsWallet,
+    gunnerTokenDistributor,
+    gunnerTokenAirdrop,
+    gunnerEmission,
+  };
+};
 
 describe('GunnerBrosERC20', () => {
-  let gunnerERC20: GunnerERC20,
-    operationsWallet: Wallet,
-    gunnerTokenDistributor,
-    gunnerTokenAirdrop
-
-  beforeEach(async () => {
-    operationsWallet = ethers.Wallet.createRandom()
-    gunnerERC20 = await (
-      await ethers.getContractFactory('GunnerERC20')
-    ).deploy()
-  })
-
   it('Can be minted only by the owner', async () => {
+    const addr = await getAddresses();
+    const signer = new ethers.Wallet(addr.gunnerEmission.privateKey, ethers.provider);
+    await ethers.provider.send('hardhat_setBalance', [
+      signer.address,
+      ethers.utils.hexStripZeros(ethers.utils.parseEther('0.1')._hex),
+    ]);
     await expect(
-      gunnerERC20.mint(
-        operationsWallet.address,
-        ethers.Wallet.createRandom().address,
-        ethers.Wallet.createRandom().address,
-        ethers.Wallet.createRandom().address,
-        { from: operationsWallet.address },
+      addr.contract
+        .connect(signer)
+        .mint(
+          addr.operationsWallet.address,
+          addr.gunnerEmission.address,
+          addr.gunnerTokenAirdrop.address,
+          addr.gunnerTokenDistributor.address,
+        ),
+    ).to.be.reverted;
+    await expect(
+      addr.contract.mint(
+        addr.operationsWallet.address,
+        addr.gunnerEmission.address,
+        addr.gunnerTokenAirdrop.address,
+        addr.gunnerTokenDistributor.address,
       ),
-    ).to.be.reverted
-  })
+    ).to.not.be.reverted;
+  });
 
-  it('Should have a 100M supply', async () => {})
-})
+  it('Should have a 100M supply', async () => {
+    const addr = await getAddresses();
+    await (
+      await addr.contract.mint(
+        addr.operationsWallet.address,
+        addr.gunnerEmission.address,
+        addr.gunnerTokenAirdrop.address,
+        addr.gunnerTokenDistributor.address,
+      )
+    ).wait();
+    expect(await addr.contract.totalSupply()).to.be.equal(ethers.BigNumber.from(10).pow(26));
+    await expect(
+      addr.contract.mint(
+        addr.operationsWallet.address,
+        addr.gunnerEmission.address,
+        addr.gunnerTokenAirdrop.address,
+        addr.gunnerTokenDistributor.address,
+      ),
+    ).to.be.reverted;
+  });
+});
