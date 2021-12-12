7d20a55dae737fabeecf54b328e1ae7ba7ad6b19
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-12 14:16:04 -0500

    Finished GunnerBrosERC20 tests

--- test/GunnerBrosERC20.test.ts
+++ test/GunnerBrosERC20.test.ts
@@ -1,7 +1,5 @@
-import { ethers } from 'hardhat';
 import { expect } from 'chai';
-import { GunnerERC20 } from '../typechain';
-import { Wallet } from '@ethersproject/wallet';
+import { ethers } from 'hardhat';
 
 const getAddresses = async () => {
   const [operationsWallet, gunnerTokenDistributor, gunnerTokenAirdrop, gunnerEmission] = [...Array(4).keys()].map((e) =>
@@ -66,5 +64,57 @@ describe('GunnerBrosERC20', () => {
         addr.gunnerTokenDistributor.address,
       ),
     ).to.be.reverted;
+  });
+
+  describe('It should send', () => {
+    const _to = (n: number) => ethers.BigNumber.from(10).pow(26).mul(n).div(100);
+    it('20M to operations', async () => {
+      const addr = await getAddresses();
+      await (
+        await addr.contract.mint(
+          addr.operationsWallet.address,
+          addr.gunnerEmission.address,
+          addr.gunnerTokenAirdrop.address,
+          addr.gunnerTokenDistributor.address,
+        )
+      ).wait();
+      expect(await addr.contract.balanceOf(addr.operationsWallet.address)).to.be.eq(_to(20));
+    });
+    it('10M to emissions', async () => {
+      const addr = await getAddresses();
+      await (
+        await addr.contract.mint(
+          addr.operationsWallet.address,
+          addr.gunnerEmission.address,
+          addr.gunnerTokenAirdrop.address,
+          addr.gunnerTokenDistributor.address,
+        )
+      ).wait();
+      expect(await addr.contract.balanceOf(addr.gunnerEmission.address)).to.be.eq(_to(10));
+    });
+    it('10M to airdrop', async () => {
+      const addr = await getAddresses();
+      await (
+        await addr.contract.mint(
+          addr.operationsWallet.address,
+          addr.gunnerEmission.address,
+          addr.gunnerTokenAirdrop.address,
+          addr.gunnerTokenDistributor.address,
+        )
+      ).wait();
+      expect(await addr.contract.balanceOf(addr.gunnerTokenAirdrop.address)).to.be.eq(_to(10));
+    });
+    it('60M to game rewards', async () => {
+      const addr = await getAddresses();
+      await (
+        await addr.contract.mint(
+          addr.operationsWallet.address,
+          addr.gunnerEmission.address,
+          addr.gunnerTokenAirdrop.address,
+          addr.gunnerTokenDistributor.address,
+        )
+      ).wait();
+      expect(await addr.contract.balanceOf(addr.gunnerTokenDistributor.address)).to.be.eq(_to(60));
+    });
   });
 });
