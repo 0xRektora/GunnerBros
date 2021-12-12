777f3b8280563289f3f4ebd80016ebab84779102
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-12 14:35:18 -0500

    + Formatted with eslint + Implemented Ownable + Updated _mint to _safeMint + Added error message ro require

--- contracts/GunnerBrosERC721.sol
+++ contracts/GunnerBrosERC721.sol
@@ -2,26 +2,27 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract GunnerBros is ERC721Enumerable {
+contract GunnerBros is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
-    Counters.Counter private tokenIds;
+    Counters.Counter public tokenIds;
 
     string private baseUri;
     uint256 constant maxCap = 9999;
 
-    constructor(string memory __baseUri) ERC721("GunnerBros", "GNB") {
+    constructor(string memory __baseUri) ERC721('GunnerBros', 'GNB') {
         baseUri = __baseUri;
     }
 
-    function mint(address _to) public returns (uint256) {
-        require(tokenIds.current() < maxCap);
+    function mint(address _to) public onlyOwner returns (uint256) {
+        require(tokenIds.current() < maxCap, 'GunnerBros::mint All NFTs have been minted');
         tokenIds.increment();
 
         uint256 newItemId = tokenIds.current();
-        _mint(_to, newItemId);
+        _safeMint(_to, newItemId);
 
         return newItemId;
     }
