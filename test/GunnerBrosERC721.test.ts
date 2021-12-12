b24d0ca75faf0710b17f6d13167ea77683bcf469
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-12 14:34:29 -0500

    Finished GunnerBrosERC721 tests

---
+++ test/GunnerBrosERC721.test.ts
@@ -0,0 +1,44 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+const getAddresses = async () => {
+  const [operationsWallet, gunnerTokenDistributor, gunnerTokenAirdrop, gunnerEmission] = [...Array(4).keys()].map((e) =>
+    ethers.Wallet.createRandom(),
+  );
+  const contract = await (await ethers.getContractFactory('GunnerBrosMock')).deploy('');
+
+  return {
+    deployer: (await ethers.getSigners())[0],
+    contract,
+    operationsWallet,
+    gunnerTokenDistributor,
+    gunnerTokenAirdrop,
+    gunnerEmission,
+  };
+};
+
+const setBalance = async (addr: string, ether: number) => {
+  await ethers.provider.send('hardhat_setBalance', [
+    addr,
+    ethers.utils.hexStripZeros(ethers.utils.parseEther(String(ether))._hex),
+  ]);
+};
+
+describe('GunnerBrosERC721', () => {
+  it('Should be minted only by owner', async () => {
+    const addr = await getAddresses();
+    const signer = new ethers.Wallet(ethers.Wallet.createRandom().privateKey, ethers.provider);
+    await setBalance(signer.address, 1);
+    await expect(addr.contract.mint(ethers.Wallet.createRandom().address)).to.be.not.reverted;
+    await expect(addr.contract.connect(signer).mint(ethers.Wallet.createRandom().address)).to.be.revertedWith(
+      'Ownable: caller is not the owner',
+    );
+  });
+  it('Should have a max cap of 9999', async () => {
+    const addr = await getAddresses();
+    await addr.contract.setTokenId(9999);
+    await expect(addr.contract.mint(ethers.Wallet.createRandom().address)).to.be.revertedWith(
+      'GunnerBros::mint All NFTs have been minted',
+    );
+  });
+});
